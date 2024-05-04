7f4fd9f5480c7495df4767f4ac4815b5b6ca5f9f
Prasad Dilip Patewar <prasadpatewar39@gmail.com>
2024-05-04 16:49:04 +0530

    Added drag functionality on card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { DragEvent, useState } from "react";
 import { listIDs } from "../types";
 import styles from "./Card.module.scss";
 
@@ -11,8 +11,25 @@ type Props = {
 const Card = ({ id, listID, title }: Props) => {
   const [onHold, setOnHold] = useState(false);
 
+  const dragStartHandler = (e: DragEvent<HTMLDivElement>) => {
+    e.dataTransfer.setData("cardInfo", JSON.stringify({ id, listID }));
+    setTimeout(() => {
+      setOnHold(true);
+    }, 0);
+  };
+
+  const dragEndHandler = () => {
+    setOnHold(false);
+  };
+
   return (
-    <div id={id} className={`${styles.card} ${onHold ? styles.hidden : ""}`}>
+    <div
+      id={id}
+      className={`${styles.card} ${onHold ? styles.hidden : ""}`}
+      draggable="true"
+      onDragStart={dragStartHandler}
+      onDragEnd={dragEndHandler}
+    >
       <h5>{title}</h5>
     </div>
   );
